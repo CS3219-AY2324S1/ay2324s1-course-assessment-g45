b67547181589a87f4c3870cd4bacd6edc7868696
pcgiang <chaugiang0512@gmail.com>
2023-09-14 17:42:59 +0800

    Add bootstrap

--- frontend-react/src/components/QuestionTable.js
+++ frontend-react/src/components/QuestionTable.js
@@ -1,37 +1,45 @@
 import React from 'react'
 import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const QuestionTable = () => {
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
+    // <Table striped bordered hover>
+    //   <thead>
+    //     <tr>
+    //       <th>#</th>
+    //       <th>First Name</th>
+    //       <th>Last Name</th>
+    //       <th>Username</th>
+    //     </tr>
+    //   </thead>
+    //   <tbody>
+    //     <tr>
+    //       <td>1</td>
+    //       <td>Mark</td>
+    //       <td>Otto</td>
+    //       <td>@mdo</td>
+    //     </tr>
+    //     <tr>
+    //       <td>2</td>
+    //       <td>Jacob</td>
+    //       <td>Thornton</td>
+    //       <td>@fat</td>
+    //     </tr>
+    //     <tr>
+    //       <td>3</td>
+    //       <td colSpan={2}>Larry the Bird</td>
+    //       <td>@twitter</td>
+    //     </tr>
+    //   </tbody>
+    // </Table>
+    <Container className='p-3 m-0 border-0 bd-example m-0 border-0 bd-example-row'>
+    <Row>
+      <Col>1 of 1</Col>
+    </Row>
+  </Container>
   );
 }
 
