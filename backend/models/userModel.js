c8fc61695351e307581af858a59c160564148777
DragonPig42 <77189300+DragonPig42@users.noreply.github.com>
2023-09-18 01:08:38 +0800

    add validator to check for strong password and valid email

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
+const validator = require("validator")
 
 const Schema = mongoose.Schema
 
@@ -22,6 +23,18 @@ const userProfileSchema = new Schema({
 
 userProfileSchema.statics.signUp = async function(username, password, email) {
     
+    if (!email || !password || !username) {
+        throw Error("All fields must be filled")
+    }
+
+    if(!validator.isEmail(email)) {
+        throw Error("Email is not valid")
+    }
+
+    if(!validator.isStrongPassword(password)) {
+        throw Error("Password not strong enough")
+    }
+
     // const exists = await this.findOne({
     //     $or: [
     //         { username: username },
