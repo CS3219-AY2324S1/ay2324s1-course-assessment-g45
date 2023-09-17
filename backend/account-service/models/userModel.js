3a9ba8dbaeda30d09264ffb7ca53de8992584599
DragonPig42 <77189300+DragonPig42@users.noreply.github.com>
2023-09-18 00:35:13 +0800

    signing up with hash password

--- backend/account-service/models/userModel.js
+++ backend/account-service/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
 
 const Schema = mongoose.Schema
 
@@ -18,5 +19,32 @@ const userProfileSchema = new Schema({
         unique: true
     }
 }, {timestamps: true})
+
+userProfileSchema.statics.signUp = async function(username, password, email) {
+    
+    // const exists = await this.findOne({
+    //     $or: [
+    //         { username: username },
+    //         { email: email }
+    //     ]
+    // });
+
+    // if (exists) {
+    //     if (exists.username === username) {
+    //         throw Error('Username already in use');
+    //     }
+    //     if (exists.email === email) {
+    //         throw Error('Email already in use');
+    //     }
+    // }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ username, password: hash, email})
+
+    return user
+
+}
 
 module.exports = mongoose.model('UserProfile', userProfileSchema)
