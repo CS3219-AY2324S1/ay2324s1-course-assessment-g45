3fe5f8eb25df17dd622edc40d5f5043a070ed925
YiFei2 <fongyifei1999@gmail.com>
2023-09-20 00:31:16 +0800

    Implement question context and delete functionality

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { UserContextProvider } from './contexts/userContext';
+import { QuestionsContextProvider } from './contexts/QuestionContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <UserContextProvider>
-    <App />
-  </UserContextProvider>
+    <QuestionsContextProvider>
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    </QuestionsContextProvider>
   </React.StrictMode>
 );
 
