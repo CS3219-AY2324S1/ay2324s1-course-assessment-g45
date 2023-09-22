597517a5ed76c6d5759806d815273e1053a37f6c
DragonPig42 <77189300+DragonPig42@users.noreply.github.com>
2023-09-23 01:18:48 +0800

    add useLogin hook

--- frontend-react/src/hooks/useLogin.js
+++ frontend-react/src/hooks/useLogin.js
@@ -2,24 +2,23 @@ import { UserContext } from "../contexts/userContext";
 import { useState } from "react";
 import { useUserContext } from "./useUserContext";
 
-export const useSignup = () => {
+export const useLogin = () => {
   const [error, setError] = useState(null)
   const [isLoading, setIsLoading] = useState(null)
   const {dispatch} = useUserContext()
 
-  const signup = async (username, password, email) => {
+  const login = async (username, password) => {
     setIsLoading(true)
     setError(null)
     ///api/user/signup
 
-    const response = await fetch('/api/userProfiles/', {
+    const response = await fetch('/api/userProfiles/login', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({username, password, email})
+        body: JSON.stringify({username, password})
     })
-
-
     const json = await response.json()
+
     if (!response.ok) {
         setIsLoading(false)
         setError(json.error)
@@ -33,5 +32,5 @@ export const useSignup = () => {
     }
   }
 
-  return {signup, isLoading, error}
+  return {login, isLoading, error}
 }
