de5f85fb94f987d511f14a1fa91206afad8559aa
DragonPig42 <77189300+DragonPig42@users.noreply.github.com>
2023-09-24 21:55:24 +0800

    protect react routes by routing them to the correct page depending on the state of the user

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,21 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import RegisterPage from './pages/RegisterPage';
 import Profile from './pages/Profile';
 import LoginPage from './pages/LoginPage';
 import NavBar from './components/NavBar';
+import { useUserContext } from './hooks/useUserContext';
 
 function App() {
+  const {user} = useUserContext();
   return (
     <div className="App">
       <BrowserRouter>
         <NavBar/>
         <div className='pages'>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/register' element={<RegisterPage/>}/>
-          <Route path='/login' element={<LoginPage/>}/>
-          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/' element={user ? <Home/> : <Navigate to= "/login" />}/>
+          <Route path='/register' element={!user ? <RegisterPage/> : <Navigate to= "/" />}/>
+          <Route path='/login' element={!user ? <LoginPage/> : <Navigate to= "/" />}/>
+          <Route path='/profile' element={user ? <Profile/> : <Navigate to= "/login" />}/>
         </Routes>
         </div>
       </BrowserRouter>
