a1300cd3a31004b777b3009c5542720574ed134c
pcgiang <chaugiang0512@gmail.com>
2023-09-30 19:16:41 +0800

    Initialise socket.io backend and frontend

---
+++ backend/collab-service/server.js
@@ -0,0 +1,50 @@
+require('dotenv').config()
+
+const express = require('express');
+const cors = require('cors');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io')
+
+const PORT = process.env.PORT || 3003;
+
+const app = express();
+
+app.use(cors());
+
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors : {
+    origin: "http://localhost:3000", // react local host
+    methods: ["GET", "POST"]
+  }
+})
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+server.listen(PORT, () => {
+  console.log(`server running at http://localhost:${PORT}`);
+});
+
+io.on("connection", (socket) => {
+  // console.log("user connected", socket.id)
+
+  socket.on("send_message", (data) => {
+    console.log(data)
+
+    // broadcast to everyone
+    socket.broadcast.emit("receive_message", data)
+
+    // broatcast to only people in the room
+    socket.to(data.roomNumber).emit("receive_message", data)
+  })
+
+  socket.on("join_room", (data) => {
+    console.log(data)
+
+    // join room with the room number in data
+    socket.join(data)
+  })
+})
--- frontend-react/src/pages/Home.js
+++ frontend-react/src/pages/Home.js
@@ -1,12 +1,14 @@
 import { useNavigate } from "react-router-dom"
 import { useUserContext } from "../hooks/useUserContext"
 import QuestionTable from "../components/question/QuestionTable"
+import CodeEditor from "../components/CodeEditor"
 
 const Home = () => {
   const { user, dispatch } = useUserContext()
   const navigate = useNavigate()
   return (
     <div>
+      <CodeEditor/>
       <QuestionTable/>
     </div>
   )
