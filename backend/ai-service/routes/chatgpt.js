d5e8ae8f867b31cc532a511aea6519a2019713da
DragonPig42 <77189300+DragonPig42@users.noreply.github.com>
2023-10-12 00:15:55 +0800

    set up backend route for ai service

---
+++ backend/ai-service/routes/chatgpt.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const {
+  generateResponse
+} = require('../controllers/chatgptController');
+const requireAuth = require('../middleware/requireAuth');
+
+const router = express.Router();
+
+// Require auth for all question routes
+router.use(requireAuth);
+
+// POST a new request
+router.post('/', generateResponse);
+
+
+module.exports = router;
---
+++ backend/ai-service/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const chatgptRoutes = require('./routes/chatgpt');
+
+const PORT = process.env.PORT || 3005;
+
+//express app
+const app = express();
+
+app.use(cors());
+
+// middleware for logging purposes, runs on every req
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use('/api/chatgpt', chatgptRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('listening on port', PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
