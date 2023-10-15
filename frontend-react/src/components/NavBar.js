f1eae092ef8e8df956f097f2267e125f2391bd02
DragonPig42 <77189300+DragonPig42@users.noreply.github.com>
2023-10-15 23:59:41 +0800

    update photo for profile pic

--- frontend-react/src/components/NavBar.js
+++ frontend-react/src/components/NavBar.js
@@ -1,10 +1,11 @@
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate} from "react-router-dom"
 import { useLogout } from "../hooks/useLogout"
 import React, { useState, useEffect, useRef } from 'react';
 import { useUserContext } from "../hooks/useUserContext"
-import logo from '../assets/images/logo.svg'
 import { useMatchContext } from '../hooks/useMatchContext';
 import WaitingBanner from "./match/WaitingBanner";
+import logo from '../assets/images/logo.svg'
+import profile from '../assets/images/profile.png'
 
 const NavBar = () => {
   const { user } = useUserContext()
@@ -24,9 +25,9 @@ const NavBar = () => {
           setOpen(false)
         }
       };
-
+  
       document.addEventListener("mousedown", handler)
-
+  
       // Cleanup event listener when component is unmounted or when user logs out
       return () => {
         document.removeEventListener("mousedown", handler)
@@ -34,7 +35,7 @@ const NavBar = () => {
     }
   }, [user]);
 
-  const handleLogout = () => {
+  const handleLogout = () => { 
     logout()
     setOpen(false)
   }
@@ -46,61 +47,60 @@ const NavBar = () => {
 
   return (
     <>
-      <nav>
+    <nav>
+ 
+     
+      <div className="navdiv" > 
+      <Link to = "/match">
+      <img className = "logoImage" src={logo}>
+            </img>      
+      </Link>
+      <h4 className="card-title ms-2" style={{color: 'white'}}>Peer Prep</h4>
+      {user &&
+        <ul id = "navbar" 
+         className = {isClicked ? "#navbar active" : "#navbar"}
+        > 
+         <li><Link className={location.pathname === "/match" ? "active" : ""}
+          to = "/match"> Home </Link></li>  
+         <li><Link className={location.pathname === "/" ? "active" : ""}
+          to = "/"> Questions </Link></li>  
+          {user && user.role === "maintainer" &&
+          <li><Link className={location.pathname === "/maintainer" ? "active" : ""}
+          to = "/maintainer"> Dashboard </Link></li>  }
+        </ul>
+         }
+      </div>  
 
-
-        <div className="navdiv" >
-          <Link to="/match">
-            <img className="logoImage" src={logo}>
+      <div>
+            {state.showBanner && <div><WaitingBanner></WaitingBanner></div>}
+        </div>
+      
+      {user &&
+      <div className='menu-container' ref={menuRef}>
+          <div className='menu-trigger' onClick={()=>{setOpen(!open)}}>
+          <img src={profile}>
             </img>
-          </Link>
-          <h4 className="card-title ms-2" style={{ color: 'white' }}>Peer Prep</h4>
-          {user &&
-            <ul id="navbar"
-              className={isClicked ? "#navbar active" : "#navbar"}
-            >
-              <li><Link className={location.pathname === "/match" ? "active" : ""}
-                to="/match"> Home </Link></li>
-              <li><Link className={location.pathname === "/" ? "active" : ""}
-                to="/"> Questions </Link></li>
-              {user && user.role === "maintainer" &&
-                <li><Link className={location.pathname === "/maintainer" ? "active" : ""}
-                  to="/maintainer"> Dashboard </Link></li>}
+          </div>
+          <div className={`dropdown-menu ${open? 'active' : 'inactive'}`}> 
+            <h3>{user.username}<br/>{user.role != "user" && <span>Role: {user.role}</span>}</h3>
+            <ul>
+              <DropdownItem img = {"https://img.icons8.com/ios-filled/50/user-male-circle.png"} text = {"My Profile"} onClick ={navigateToProfile}/>
+              <DropdownItem img = {"https://img.icons8.com/ios-filled/50/logout-rounded-left.png"} text = {"Logout"} onClick ={handleLogout}/>
             </ul>
-          }
-        </div>
-
-        <div>
-          {state.showBanner && <div><WaitingBanner></WaitingBanner></div>}
-        </div>
-
-        {user &&
-          <div className='menu-container' ref={menuRef}>
-            <div className='menu-trigger' onClick={() => { setOpen(!open) }}>
-              <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350">
-              </img>
-            </div>
-            <div className={`dropdown-menu ${open ? 'active' : 'inactive'}`}>
-              <h3>{user.username}<br />{user.role != "user" && <span>Role: {user.role}</span>}</h3>
-              <ul>
-                <DropdownItem img={"https://img.icons8.com/ios-filled/50/user-male-circle.png"} text={"My Profile"} onClick={navigateToProfile} />
-                <DropdownItem img={"https://img.icons8.com/ios-filled/50/logout-rounded-left.png"} text={"Logout"} onClick={handleLogout} />
-              </ul>
-            </div>
           </div>
-        }
-
-        {user &&
-          <div id="collapse">
-            <i id="bar"
-              className={isClicked ? "fas fa-times" : "fas fa-bars"}
-              onClick={() => setIsClicked(prevState => !prevState)}
-            >
-            </i>
-          </div>
-        }
+      </div>
+    }
+   {user &&
+      <div id = "collapse"> 
+        <i id = "bar"
+        className = {isClicked ? "fas fa-times" : "fas fa-bars"}
+        onClick={() => setIsClicked(prevState => !prevState)}
+        >
+        </i>
+      </div>
+    }
       </nav>
-    </>
+      </>
   )
 }
 
