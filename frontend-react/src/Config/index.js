9b0e162c1303074acda39d8335ee4bbdbe0c4903
Ernest <hltc11032000@gmail.com>
2023-11-08 23:46:37 +0800

    Test deployment to production

--- frontend-react/src/Config/index.js
+++ frontend-react/src/Config/index.js
@@ -7,20 +7,35 @@ const Config = {
   Common: {
     QuestionApiBaseUrl:
       environment === Environment.production
-        ? 'http://localhost:3002'
+        ? process.env.QUESTION_SERVICE_URL
         : 'http://localhost:3002',
     UserApiBaseUrl:
       environment === Environment.production
-        ? 'http://localhost:3001'
+        ? process.env.USER_SERVICE_URL
         : 'http://localhost:3001',
     CollabSessionApiBaseUrl:
-      'http://localhost:3003',
+      environment === Environment.production
+        ? process.env.COLLAB_SERVICE_URL
+        : 'http://localhost:3003',
     MatchingApiBaseUrl:
-      'http://localhost:3004',
+      environment === Environment.production
+        ? process.env.MATCHING_SERVICE_URL
+        : 'http://localhost:3004',
     ChatGPTApiBaseUrl:
       environment === Environment.production
-        ? 'http://localhost:3006'
-        : 'http://localhost:3006'
+        ? process.env.AI_SERVICE_URL
+        : 'http://localhost:3006',
+
+    // QuestionApiBaseUrl:
+    //   'https://question-g5qibfavfa-uc.a.run.app',
+    // UserApiBaseUrl:
+    //   'https://user-g5qibfavfa-uc.a.run.app',
+    // CollabSessionApiBaseUrl:
+    //   'https://collab-g5qibfavfa-uc.a.run.app',
+    // MatchingApiBaseUrl:
+    //   'https://matching-g5qibfavfa-uc.a.run.app',
+    // ChatGPTApiBaseUrl:
+    //   'https://ai-g5qibfavfa-uc.a.run.app'
   },
 };
 
